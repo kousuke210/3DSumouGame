e00a9b926b2c297215f2af83897f3346f031a3e4
TIO-35 <reregaga28@gmail.com>
2026-06-30 15:48:18 +0900

    try10000

---
+++ 3DSumouGame/Assets/sumou.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class sumou : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //みんなも魔法少女プリティベルを読もう！！
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
